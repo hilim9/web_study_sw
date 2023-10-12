5d940070635fc86391598edece233921001e2a40
hilim9 <dlagydls1212@naver.com>
2023-10-12 17:52:55 +0900

    day04

--- day04/src/main/java/servlets/HelloServlet.java
+++ day04/src/main/java/servlets/HelloServlet.java
@@ -10,8 +10,20 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
 
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         // super.doGet(req, resp); -> 유지시 동작 안함
-        System.out.println("안녕하세요");
+        //System.out.println("안녕하세요");
+        System.out.println("doGet!!");
+    }
+
+    @Override
+    public void destroy() { // 가장 마지막에 한번만 호출
+        System.out.println("destroy!!");
+    }
+
+    @Override
+    public void init() throws ServletException { // 가장 처음 한번만 호출
+        System.out.println("init!!");
     }
 }
