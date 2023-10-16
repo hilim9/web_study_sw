ecbad0653728cbe418474635cad0ad2b2b6f7168
hilim9 <dlagydls1212@naver.com>
2023-10-16 17:53:09 +0900

    day05

---
+++ day05/src/main/java/models/members/Member.java
@@ -0,0 +1,17 @@
+package models.members;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private String userId;
+
+    private String userPw;
+    private String confirmUserPw;
+
+    private String userNm;
+    private String email;
+
+
+}
